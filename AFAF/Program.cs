626c47ec5a8edfc4dc27dc3355c7077c56f98ac8
Scott Hanselman <scott@hanselman.com>
2021-12-14 13:08:21 -0800

    almost there

--- AFAF/Program.cs
+++ AFAF/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
 using System.Text.Json;
 using System.Text.Json.Nodes;
 
@@ -9,35 +10,61 @@ HttpClient client = new HttpClient();
 // Our "base" URL is Production
 client.BaseAddress = new Uri("https://docs.microsoft.com");
 int pageNumber = 0;
+int totalCount = 0;
 
-string jsonString = await client.GetStringAsync(String.Format(urlMain, pageNumber));
-var jsonObject = JsonNode.Parse(jsonString);
-var totalCount = (int)jsonObject["totalCount"].AsValue();
+Dictionary<string, Episode> episodes =  new Dictionary<string, Episode>();
 
-foreach(JsonObject item in jsonObject["episodes"].AsArray())
+
+while (true)
 {
-    var show = JsonSerializer.Deserialize<Show>(item);
-    Console.WriteLine(show.title);
+    string epUrl = String.Format(urlMain, pageNumber);
+    string jsonString = await client.GetStringAsync(epUrl);
+    Console.WriteLine($"Fetching {epUrl}");
+    var jsonObject = JsonNode.Parse(jsonString);
+    totalCount = (int)jsonObject["totalCount"].AsValue(); //don't need to do this twice
+
+    JsonNode epNode = jsonObject["episodes"]; 
+    if (epNode?.AsArray() != null && epNode.AsArray().Count == 0) break;
+
+    StringBuilder batchEps = new StringBuilder();
+
+    foreach (JsonObject item in epNode.AsArray())
+    {
+        var ep = JsonSerializer.Deserialize<Episode>(item);
+
+        batchEps.Append(ep.entryId + ",");
+        episodes.Add(ep.entryId, ep);
+    }
+    batchEps.ToString().TrimEnd(','); //toss ending ,
+
+    string epDetailsUrl = String.Format(urlBatch, batchEps);
+    string jsonDetailsString = await client.GetStringAsync(epDetailsUrl);
+
+    //get thumbnail and youtube url if they are there
+    // go get each episode by guid and update with youtube and thumbnail
+
+
+    pageNumber++;
 }
+
 Console.ReadLine();
 
+//save a json file with all details (format=taco?)
+// save to azure storage?
 
 
 
 
-
-
-
-
-
-
-
-public record Show
+public record Episode
 {
     public string title { get; init; }
     public string url { get; init; }
     public string description { get; init; }
     public string entryId { get; init; }
-}
+    public DateTime uploadDate { get; init; }
 
+    //from details
+    public string youTubeUrl { get; init; }
+    public string thumbnailUrl { get; init; }
+}
 
